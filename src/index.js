651ff59ef4e46fda24a8955fc0ea52ff1be4141e
Kennedy Rose <kennedy@kennedyrose.com>
2018-06-04 15:34:38 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class LinkedinShare extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			link: props.link
+		}
+	}
+	componentDidMount() {
+		if (!this.state.link) {
+			this.setState({
+				link: document.location.href
+			})
+		}
+	}
+	componentDidReceiveProps() {
+		if (this.props.link) {
+			this.setState({
+				link: this.props.link
+			})
+		}
+	}
+	render() {
+		return this.props.children(`https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(this.state.link)}&title=${this.props.title}&summary=${this.props.summary}&source=`)
+	}
+}
+
+LinkedinShare.defaultProps = {
+	title: ``,
+	summary: ``,
+}
+
+export default LinkedinShare
